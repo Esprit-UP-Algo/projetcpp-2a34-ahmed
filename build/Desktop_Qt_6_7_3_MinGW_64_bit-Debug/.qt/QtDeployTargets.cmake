8e786b84dc82d489c17a2b4bbf3c403e97d709df
ahmedzarrami <Ahmed.Zarrami@esprit.tn>
2025-10-03 23:55:31 +0100

    Supprimer les fichiers générés et ajouter .gitignore

--- build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_surface_yassine_FILE C:/Users/zarrami/Documents/untitled3/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/surface_yassine.exe)
-set(__QT_DEPLOY_TARGET_surface_yassine_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_surface_yassine_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
